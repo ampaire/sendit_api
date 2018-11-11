51b30ef30c89e3cdd30a57abbd59243f4c642e45
ampaire <femayapr@gmail.com>
2018-11-11 21:17:11 +0300

    add updates to the cancel order route and get all parcels endpoints[DELIVERS #161841704,#161841564]

--- api/models.py
+++ api/models.py
@@ -1,42 +1,35 @@
-from flask import Flask, request, jsonify
 import datetime
 
 
+class Users:
+    users = []
+
+    def __init__(self, name, email, password):
+        if (username != '' and email != ''
+                and password != '') or (username is not None
+                                        and email is not None
+                                        and password is not None):
+            self.userId = userId
+            self.name = name
+            self.email = email
+            self.password = password
+        else:
+            raise ValueError(
+                "make sure the name, email and password fields are filled")
 
 
-parcels = []
+class Parcels:
+    parcels = []
 
-def create_parcels():
-
-
-    parcel = {
-        "deliveryId": len(parcels)+1,
-        "date": datetime.datetime.utcnow(),
-        "location": request.get_json().get('location'),
-        "destination": request.get_json().get('destination'),
-        "weight": request.get_json().get('weight')
-    }
-    for parcel in parcels:
-        if parcel in parcels:
-            return jsonify({"message":"Oops! delivery order seems to exists"})
-        if len(parcel) == 0:
-            return jsonify({"message":"Cannot add empty parcel"})
-    
-    parcels.append(parcel)
-
-    return jsonify({"parcels": parcels})
-
-def get_parcel_orders():
-    if len(parcels) < 1:
-        return jsonify({'message': 'There is no data to display, add a delivery order'}), 400
-    
-        return jsonify({'parcels': parcels}), 200
-
-
-def delete_parcel():
-    parcel = [parcel for parcel in parcels if parcel['deliveryId'] == deliveryId]
-    if len(parcel) == 0:
-        return ("Failed to Delete! Cannot delete non-existent item")
-    parcels.remove(parcel[0])
-    return jsonify({'parcels': parcels, "message": 'Successfully Deleted'}), 200
-
+    def __init__(self, pickup_location, destination, recipient, description):
+        if (pickup_location != '' and destination != ''
+                and recipient != '' and description != "" and status != "") or (pickup_location is not None
+                                                                                and destination is not None
+                                                                                and recipient is not None
+                                                                                and description is not None
+                                                                                and status is not None):
+        self. parcelId = parcelId
+        self.pickup_location = pickup_location
+        self.destination = destination
+        self.recipient = recipient
+        self.description = description
--- tests/test_parcel.py
+++ tests/test_parcel.py
@@ -7,19 +7,42 @@ class TestParcels(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client(self)
         self.client.testing = True
+        self.delivery_order = {
+            "pickup_location": "kampala",
+            "destination": "mbarara",
+            "recipient": "Mr. Mugisha"
+            "weight": "1.2kgs"
+        }
+
+    def test_can_add_delivery_order(self):
+        response = self.client.post(
+            "/api/v1/parcels", data=json.dumps(self.delivery_order), content_type="application/JSON")
+        self.assertEqual(response.status_code, 201)
 
     def test_can_get_parcels(self):
         response = self.client.get('/api/v1/parcels')
         self.assertEqual(response.status_code, 200)
 
+    def test_can_get_one_parcel_delivery_order(self):
+        response = self.client.get('/api/v1/parcels/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_cannot_get_non_existing(self):
+        response = self.client.get('/api/v1/parcels/again')
+        self.assertEqual(response.status_code, 404)
+
     def test_delete(self):
         response = self.client.delete("/api/v1/parcels/1")
         self.assertEqual(response.status_code, 200)
-        
+
     def test_cannot_delete_non_existent(self):
         response = self.client.delete("/api/v1/parcels/u")
         self.assertEqual(response.status_code, 404)
 
-    
+    def test_invalid_JSON(self):
+        response = self.client.post('/api/v1/parcels/1',
+                                    data="This is a string!",
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 405)
 
     
